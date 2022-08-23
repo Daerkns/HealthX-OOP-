6ae254d89b236aa089273422de3255405e402daf
Souvik Ahmed <77106829+Daerkns@users.noreply.github.com>
2022-08-23 06:58:56 +0600

    Getting Main class up and running

---
+++ main.py
@@ -0,0 +1,30 @@
+from login import loginsystem
+
+
+def main():
+
+    account = loginsystem()
+
+    while True:
+        print("""
+        ======  Welcome to HealthX  ======
+        1. Create New Account
+        2. Sign in to your Account
+        """)
+
+        choice = input("Enter Choice 1/2 : ")
+
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("Please enter an int")
+            continue
+
+        if choice == 1:
+            account.signup()
+
+        elif choice == 2:
+            account.login()
+
+        else:
+            print("Invalid Input. Please enter 1 or 2.")
